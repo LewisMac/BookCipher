afaea9713a7538f506823d3b80a27ecc1354f10c
Lewis MacNee <lewis.macnee@hotmail.com>
2018-01-29 14:35:56 +0000

    Added Test.txt file with random testing words inside, added error message return if unable to open file.

--- BookCipherEncoderDecoder/BookCipherEncoderDecoder.cpp
+++ BookCipherEncoderDecoder/BookCipherEncoderDecoder.cpp
@@ -3,8 +3,25 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+	int count = 0;
+	int exampleNumToFind = 12;
+
+	ifstream inFile;
+
+	infile.open("test.txt");
+	if (!inFile) {
+		cout << "Unable to open file";
+		exit(1);
+		//will terminate process with error
+	}
+
 	return 0;
 }
